2b83dff66cd0eef82aaec4466a385b2d9b817acc
msarpong <msarpong1991@gmail.com>
2022-07-14 16:56:40 +0200

    CHANGED adapter with AsynDiff

--- app/src/main/java/com/msarpong/arch/adapter/MainAdapter.kt
+++ app/src/main/java/com/msarpong/arch/adapter/MainAdapter.kt
@@ -2,12 +2,26 @@ package com.msarpong.arch.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.msarpong.arch.data.ListItem
 import com.msarpong.arch.databinding.ItemListBinding
 
-class MainAdapter : ListAdapter<ListItem, ListViewHolder>(DiffUtil()) {
+class MainAdapter : RecyclerView.Adapter<ListViewHolder>() {
+
+    private val DIFF_CALLBACK: DiffUtil.ItemCallback<ListItem> =
+        object : DiffUtil.ItemCallback<ListItem>() {
+            override fun areItemsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+
+    private val mDiffer: AsyncListDiffer<ListItem> = AsyncListDiffer(this, DIFF_CALLBACK)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val binding = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -15,8 +29,17 @@ class MainAdapter : ListAdapter<ListItem, ListViewHolder>(DiffUtil()) {
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
+//        val item = getItem(position)
+//        holder.bind(item)
+        holder.bind(mDiffer.currentList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mDiffer.currentList.size;
+    }
+
+    fun submitList(list: List<ListItem>) {
+        return mDiffer.submitList(list)
     }
 }
 
@@ -26,15 +49,5 @@ class ListViewHolder(private val binding: ItemListBinding) : RecyclerView.ViewHo
     fun bind(item: ListItem) {
         name.text = item.name
         first.text = item.First
-    }
-}
-
-class DiffUtil : androidx.recyclerview.widget.DiffUtil.ItemCallback<ListItem>() {
-    override fun areItemsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
-        return oldItem == newItem
     }
 }
